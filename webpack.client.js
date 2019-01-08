c6d5d4b8a9c96e2fc32aa48fb8d98ad9c397ee02
Vignesh S <vignesh6993@live.in>
2019-01-08 11:38:44 +0530

    07-01-19 commit 20%

---
+++ webpack.client.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/client/client.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', {
+              targets: {
+                browsers: ['last 2 versions']
+              }
+            }]
+          ]
+        }
+      }
+    ]
+  }
+};
